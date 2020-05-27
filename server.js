a81404f1b2d935eb9889a16d7f88ff21883453c0
Ben Freudman <bfreud94@gmail.com>
2020-05-27 12:30:12 -0400

    more changes to prepare for heroku deploy

--- server.js
+++ server.js
@@ -21,9 +21,14 @@ app.use(cors());
 // Use NovelCovid
 const novelCovid = new NovelCovid();
 
+// Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('coronavirus-data-tracker-client/build'))
+    app.use(express.static('coronavirus-data-tracker-client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'coronavirus-data-tracker-client', 'build', 'index.html'));
+    });
 }
 
 // Starting server
