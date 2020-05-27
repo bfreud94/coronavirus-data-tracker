7e78a5b38b3ef47b3d0397352875d8f501ceadb7
Ben Freudman <bfreud94@gmail.com>
2020-05-27 19:05:42 -0400

    another heroku deploy attempt

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ const novelCovid = new NovelCovid();
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('coronavirus-data-tracker-client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'coronavirus-data-tracker-client', 'build', 'index.html'));
-    });
 }
 
 // Starting server
