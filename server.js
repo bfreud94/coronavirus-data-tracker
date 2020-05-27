0e37dbbbd09940e9ed41992da2fc01e1b8bf6031
Ben Freudman <bfreud94@gmail.com>
2020-05-27 18:57:59 -0400

    trying again to deploy to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const app = express();
 
 // Port number
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 // Use bodyParser
 app.use(bodyParser.json());
