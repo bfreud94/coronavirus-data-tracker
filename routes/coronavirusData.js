3285adc452f9a24c1e66bb3a72ee05ba0740bc36
bfreud94 <bfreud94@gmail.com>
2021-09-03 17:53:27 -0400

    updated marginalDataUSA api

--- routes/coronavirusData.js
+++ routes/coronavirusData.js
@@ -26,13 +26,22 @@ router.get('/totalDataUSA', async (request, response, next) => {
 
 router.get('/marginalDataUSA', async (request, response, next) => {
     try {
-        let data = await (await fetch('https://api.covidtracking.com/v1/us/daily.json')).json();
-        data = data.map((item) => ({
-            date: convertToDate(item.date),
-            cases: item.positiveIncrease === null ? 0 : item.positiveIncrease,
-            deaths: item.deathIncrease === null ? 0 : item.deathIncrease
+        let data = await (await fetch('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv')).text();
+        data = data.split('\n').map((item) => {
+            const [date, cases, deaths] = item.split(',');
+            return {
+                date,
+                cases: cases === '' ? 0 : parseInt(cases, 10),
+                deaths: deaths === '' ? 0 : parseInt(deaths, 10)
+            };
+        });
+        data.shift();
+        data = data.map(({ date, cases, deaths }, i) => ({
+            date,
+            cases: i === 0 ? 0 : cases - data[i - 1].cases,
+            deaths: i === 0 ? 0 : deaths - data[i - 1].deaths
         }));
-        response.send(data.reverse());
+        response.send(data);
     } catch (error) {
         next(error);
     }
